c110a91c4528fa430a009b0e02d50b78b9c9588a
Гафур Шарипов / Feyalon <63968483+Feyalon@users.noreply.github.com>
2023-02-07 20:34:20 +0500

    Update task3.js

--- task3.js
+++ task3.js
@@ -1,25 +1,19 @@
 // Задание 3. 11:30 - 12:20
-function IntPad(int){
-    const Num = String(int);
-    const NumArr = [];
-    for (var i = 0; i < Num.length; i++) {
-        NumArr.push(Num.charAt(i));
-    }
+function intPad(int){
+    const num = String(int)
     
-    const end = NumArr[NumArr.length-1]; 
-    const penult = NumArr[NumArr.length-2];
+    const end = Number([num[num.length-1]]); 
+    const penult = Number([num[num.length-2]]);
     
-    if ((end >=5 && end <= 9) ||  (end === "0") ||
+    if ((end >=5 && end <= 9) ||  (end === 0) ||
         (end ==1 && penult == 1) ||
         (end >=2 && end <= 4 && penult == 1))
     {
         console.log (int +" компьютеров");
-    }
-        
+    } 
     if (end == 1 && penult !=1) {
         console.log (int + " компьютер")
     }
-    
     if (end >=2 && end <= 4 && penult != 1) {
         console.log(int + " компьютера")
     }
